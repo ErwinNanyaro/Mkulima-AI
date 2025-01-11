ea3f5e34f4c1f29cfd57033d833aa43b35f49e9d
ErwinNanyaro <erwinsebastian320@gmail.com>
2025-01-11 16:20:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Smooth scrolling for menu links
 document.querySelectorAll(".menu-content a").forEach(link => {
   link.addEventListener("click", function (e) {
-    e.preventDefault(); // Prevent the default link action
-    const targetId = this.getAttribute("href").slice(1); // Get the target section ID
-    document.getElementById(targetId).scrollIntoView({ behavior: "smooth" }); // Smooth scroll to the section
+    e.preventDefault();
+    const targetId = this.getAttribute("href").slice(1);
+    document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
   });
 });
