014c81eabc02e892568d22bc25e1247dc88cf059
ErwinNanyaro <erwinsebastian320@gmail.com>
2025-01-11 16:22:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Smooth scrolling for menu links
 document.querySelectorAll(".menu-content a").forEach(link => {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute("href").slice(1);
-    document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+    e.preventDefault(); // Prevent the default link action
+    const targetId = this.getAttribute("href").slice(1); // Get the target section ID
+    document.getElementById(targetId).scrollIntoView({ behavior: "smooth" }); // Smooth scroll to the section
   });
 });
