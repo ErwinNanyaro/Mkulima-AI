4d6c4a49f8bcd3b2d983a4562815c52da1058c15
ErwinNanyaro <erwinsebastian320@gmail.com>
2025-01-12 19:21:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("#menu ul li a");
   const sections = document.querySelectorAll("main section");
 
-  // Toggle menu visibility
   menuButton.addEventListener("click", () => {
     menu.classList.toggle("hidden");
   });
 
-  // Show corresponding section on link click
   links.forEach((link) => {
     link.addEventListener("click", function (e) {
-      e.preventDefault(); // Prevent default behavior
+      e.preventDefault();
       const targetId = this.getAttribute("href").substring(1);
 
-      // Hide all sections
       sections.forEach((section) => {
         section.classList.add("hidden");
       });
 
-      // Show target section
       const targetSection = document.getElementById(targetId);
       if (targetSection) {
         targetSection.classList.remove("hidden");
       }
 
-      // Hide the menu
       menu.classList.add("hidden");
     });
   });
